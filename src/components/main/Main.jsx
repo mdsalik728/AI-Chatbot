aeed063261d090be5bb99d9d915fbbbafaf1cff9
mdsalik728 <62928121+mdsalik728@users.noreply.github.com>
2025-06-23 16:30:56 +0530

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
   return (
     <div className='main'>
         <div className="nav">
-            <p>Gemini</p>
+            <p>AI chatBot</p>
             <img src={assets.user_icon} alt="" />
         </div>
         <div className="main-container">
